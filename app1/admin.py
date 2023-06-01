66a85ad38b447f1947629cfaa78263b4b6a2103c
Dhruv Padhariya <dhruvpadhariya27@gmail.com>
2023-06-01 22:11:31 +0530

    commit

--- app1/admin.py
+++ app1/admin.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from .models import *
 
 
-# class CatAdmin(admin.ModelAdmin):
-#     list_display = ('img_return', 'cat_name', 'cat_desc', )
+class CatAdmin(admin.ModelAdmin):
+    list_display = ('img_return', 'cat_name', 'cat_desc', )
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -17,6 +17,6 @@ class contactRegister(admin.ModelAdmin):
     list_display = ['name',  'contact_no', 'subject',]
 
 
-# admin.site.register(categories, CatAdmin)
+admin.site.register(categories, CatAdmin)
 admin.site.register(products, ProductAdmin)
 admin.site.register(contact, contactRegister)
--- app1/models.py
+++ app1/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.utils.html import format_html
 
 # Create your models here.
+class categories(models.Model):
+    cat_id=models.IntegerField(unique=True)
+    cat_name = models.CharField(max_length=50)
+    cat_desc = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='category_image/')
+    def img_return(self):
+        return format_html('<img src="/media/{}" style="width:40px; height:40px border-radius:70%" />'.format(self.img))
+    
+    def __str__(self):
+        return self.cat_name
+    
 
 
 class products(models.Model):
@@ -10,10 +21,13 @@ class products(models.Model):
     pro_name = models.CharField(max_length=200)
     Pro_Description = models.CharField(max_length=2000, default='Default description')
     size = models.IntegerField(null=True)
-    # pro_cat = models.ForeignKey(categories, on_delete=models.CASCADE, related_name='products')
+    
+    pro_cat = models.ForeignKey(categories, on_delete=models.CASCADE, related_name='products', default='product')
     price = models.IntegerField()
     img = models.ImageField(upload_to='ProductImage',null=True, blank=True)
     
+    def __str__(self):
+        return self.title
     
 class contact(models.Model):
     
